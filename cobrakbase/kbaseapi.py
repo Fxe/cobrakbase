210e86d188db927727ee776863b70cef37f5f5a8
Christopher Henry <chenry@mcs.anl.gov>
2020-09-02 10:31:21 -0500

    Cleaning up the API adding more flexible ID processing, consolidating code, and adding a retry to get_objects

--- cobrakbase/kbaseapi.py
+++ cobrakbase/kbaseapi.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from pathlib import Path
 from cobrakbase.Workspace.WorkspaceClient import Workspace as WorkspaceClient
 from cobrakbase.kbase_object_info import KBaseObjectInfo
@@ -7,11 +8,9 @@ from cobrakbase.core.kbase_object_factory import KBaseObjectFactory
 KBASE_WS_URL = "https://kbase.us/services/ws/"
 DEV_KBASE_WS_URL = "https://appdev.kbase.us/services/ws/"
 
-
 def _get_object_wsc(wclient, oid, ws):
     res = wclient.get_objects2({"objects" : [{"name" : oid, "workspace" : ws}]})
     return res["data"][0]["data"]
-
 
 def _get_ws_client(token, dev=False):
     url = KBASE_WS_URL
@@ -34,10 +33,8 @@ class KBaseAPI:
         else:
             self.ws_client = WorkspaceClient(config['workspace-url'], token=token)
 
-    def get_object(self, object_id, ws):
-        return _get_object_wsc(self.ws_client, object_id, ws)
-
-    def get_from_ws(self, id_or_ref, workspace=None):
+    #IDs should always be processed through this function so we can interchangeably use refs, IDs, and names for workspaces and objects
+    def process_workspace_identifiers(self,id_or_ref, workspace=None):
         objspec = {}
         if workspace is None:
             objspec["ref"] = id_or_ref
@@ -50,14 +47,30 @@ class KBaseAPI:
                 objspec['objid'] = id_or_ref
             else:
                 objspec['name'] = id_or_ref
+        return objspec
+    
+    #All functions calling get_objects2 should call this function to ensure they get the retry code because workspace periodically times out
+    def get_object2(self,args):
+        tries = 0
+        while tries < 3:
+            try:
+                return self.ws_client.get_objects2(args)
+            except:
+                print("Workspace get_objects2 call failed. Trying again!")
+                tries += 1
+                time.sleep(10)
+        raise Exception("get_objects2 failed after multiple tries")
+       
+    def get_object(self, object_id, ws):
+        return self.get_object2(self.process_workspace_identifiers(object_id,ws))["data"][0]["data"]
 
-        output = self.ws_client.get_objects2({"objects": [objspec]})
+    def get_from_ws(self, id_or_ref, workspace=None):
+        output = self.get_objects2(self.process_workspace_identifiers(id_or_ref,workspace))
         factory = KBaseObjectFactory()
         return factory.create(output, None)
 
     def get_object_by_ref(self, object_ref):
-        object_info = self.get_object_info_from_ref(object_ref)
-        return self.get_object(object_info.id, object_info.workspace_id)
+        return self.get_object(object_ref, None)
     
     def save_object(self, object_id, ws, object_type, data):
         from cobrakbase import __version__
